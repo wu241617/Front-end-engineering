e01432e3ba9fa793d381640e16a0793ccb5875ad
wu241617 <1272689028@qq.com>
2020-04-12 21:39:20 +0800

    webpack/element-ui/vue-cli

---
+++ 06Vue单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1> Hello World!!! </h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {};
+		},
+		methods:{}
+	}
+</script>
+
+<style scoped="scoped">
+	h1{
+		color:red;
+	}
+</style>
